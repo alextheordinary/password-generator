dc828168cbdf1dbbb70b279ff385a4293bdc949c
Alex Bellamy <alexjbellamy@gmail.com>
2022-07-21 14:12:01 -0400

    resolved issue by removing recursive function calls and using a while loop

--- script.js
+++ script.js
@@ -15,11 +15,11 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   // Variables for criteria prompts
-  var pwLength; // Length of password 8 to 128 characters is valid
-  var lowercaseBool; // boolean for including lowercase characters
-  var uppercaseBool; // boolean for including uppercase characters
-  var numericBool; // boolean for including numeric characters
-  var specialBool; // boolean for including special characters
+  var pwLength = 0; // Length of password 8 to 128 characters is valid
+  var lowercaseBool = false; // boolean for including lowercase characters
+  var uppercaseBool = false; // boolean for including uppercase characters
+  var numericBool = false; // boolean for including numeric characters
+  var specialBool = false; // boolean for including special characters
 
   // Variables for valid characters for each type
   var lowercaseString = "abcdefghijklmnopqrstuvwxyz";
@@ -31,76 +31,68 @@ function generatePassword() {
   // Array of available characters for random generation
   var availableArray;
   // password string that will store random characters and be returned at end
-  var passwordString ;
+  var passwordString = "";
   var randomAvailableIndex; // random index value of availableArray
+  var conditionsMet = false;
 
-  // Prompt for password length 
-  pwLength = prompt("Enter a password length between 8 and 128 characters:");
+  // keep looping until parameter conditions are met or cancel is selected on password length prompt
+  while (conditionsMet === false) {
 
-  // Check for null value and return if null
+    // Prompt for password length 
+    pwLength = prompt("Enter a password length between 8 and 128 characters:");
 
-  if (pwLength === null) {
-    return;
-  }
+    // Check for null value and return if null
 
-  pwLength = Number(pwLength);  // Converting length to a number
-
-  // Check if it's a number between 8 and 128
-  // If true, continue with prompts. Otherwise, alert and reprompt for length
-
-  if ((pwLength >= 8) && (pwLength <= 128)) {
-    // Prompt for parameters
-    lowercaseBool = confirm("Include lowercase characters?");
-    uppercaseBool = confirm("Include uppercase characters?");
-    numericBool = confirm("Include numeric characters?");
-    specialBool = confirm("Include special characters?");
-
-    // Check to see if at least 1 parameter is true. Continue if true else alert and restart process
-
-    if (lowercaseBool || uppercaseBool || numericBool || specialBool) {
-      // Concatenate characters to availableCharacters string if its boolean is true
-      if (lowercaseBool) {
-        availableCharacters = availableCharacters.concat(lowercaseString);
-      }
-      if (uppercaseBool) {
-        availableCharacters = availableCharacters.concat(uppercaseString);
-      }
-      if (numericBool) {
-        availableCharacters = availableCharacters.concat(numericString);
-      }
-      if (specialBool) {
-        availableCharacters = availableCharacters.concat(specialString);
-      }
-      console.log("available chars " + availableCharacters);
-      // Put character set into an array
-      availableArray = availableCharacters.split('');
-      console.log("array length " + availableArray.length);
-      passwordString = "" ;
-
-      // Use a for loop to generate password
-      // Will run from 0 until it hits the value of pwLength variable
-      // Each character in the string will be randomly selected from availableCharacters array
-
-      for (var i = 0; i < pwLength; i++) {
-        randomAvailableIndex = Math.floor(Math.random() * availableArray.length);
-        console.log("index " + randomAvailableIndex) ;
-        console.log("value " + availableArray[randomAvailableIndex]) ;
-        console.log("pw string " + passwordString) ; 
-        passwordString = passwordString.concat(availableArray[randomAvailableIndex]);
-      }
-
-      console.log("final pw string " + passwordString) ;
-      return passwordString;
-
-    } else {
-      alert("At least one character type must be selected");
-      generatePassword();
+    if (pwLength === null) {
+      return '';
     }
 
+    pwLength = Number(pwLength);  // Converting length to a number
 
+    // Check if it's a number between 8 and 128
+    // If true, continue with prompts. Otherwise, alert and reprompt for length
 
-  } else {
-    alert("Password length must be a number between 8 and 128");
-    generatePassword();
+    if ((pwLength >= 8) && (pwLength <= 128)) {
+      // Prompt for parameters
+      lowercaseBool = confirm("Include lowercase characters?");
+      uppercaseBool = confirm("Include uppercase characters?");
+      numericBool = confirm("Include numeric characters?");
+      specialBool = confirm("Include special characters?");
+
+      // Check to see if at least 1 parameter is true. Continue if true else alert and restart process
+      if (lowercaseBool || uppercaseBool || numericBool || specialBool) {
+        conditionsMet = true;
+        // Concatenate characters to availableCharacters string if its boolean is true
+        if (lowercaseBool) {
+          availableCharacters = availableCharacters.concat(lowercaseString);
+        }
+        if (uppercaseBool) {
+          availableCharacters = availableCharacters.concat(uppercaseString);
+        }
+        if (numericBool) {
+          availableCharacters = availableCharacters.concat(numericString);
+        }
+        if (specialBool) {
+          availableCharacters = availableCharacters.concat(specialString);
+        }
+        // Put available characters set into an array
+        availableArray = availableCharacters.split('');
+
+        // Use a for loop to generate password
+        // Will run from 0 until it hits the value of pwLength variable
+        // Each character in the string will be randomly selected from availableCharacters array
+
+        for (var i = 0; i < pwLength; i++) {
+          randomAvailableIndex = Math.floor(Math.random() * availableArray.length);
+          passwordString = passwordString.concat(availableArray[randomAvailableIndex]);
+        }
+        // Returns the password string to whatever called this function
+        return passwordString;
+      } else {
+        alert("At least one character type must be selected");
+      }
+    } else {
+      alert("Password length must be a number between 8 and 128");
+    }
   }
 }
